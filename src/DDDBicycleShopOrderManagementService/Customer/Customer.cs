f0dd10de1e0b4662b041bfd442353bdfe291310e
Carl Burks <carlb@magenic.com>
2021-06-01 18:37:30 -0500

    Add test project, add webapi, add swagger

--- src/DDDBicycleShopOrderManagementService/Customer/Customer.cs
+++ src/DDDBicycleShopOrderManagementService/Customer/Customer.cs
@@ -2,12 +2,27 @@ using Common.SeedWork;
 
 namespace  DDDBicycleShopOrderManagementService.Customer {
     public class Customer: Entity, IAggregateRoot {
-        public Customer(int id){
+        public string Name { get; private set; }
+        public Company Company { get; private set; }
+        public EmailAddress PrimaryEmailAddress { get; private set; }
+        public PhysicalAddress PrimaryBillingAddress { get; private set; }
+        public Customer(int id, string name, string company, string emailaddress){
             //Skipping implementation
-        }
-        
-        //public int GetCustomerId(){
+            Id = id;
+            Name = name;
+            Company = new Company(company);
             
+        }
+        //TODO:
+        //public PhysicalAddress VerifyOrAddBillingAddress(string street, string streetAdditional, string countryCode, string state, string city, string postal)
+        //{
+        //}
+        //public PhysicalAddress VerifyOrAddShippingAddress(string street, string streetAdditional, string countryCode, string state, string city, string postal)
+        //{
+        //}
+
+        //public int GetCustomerId(){
+
         //}
 
         //public Address GetPrimaryMailingAddress(){
